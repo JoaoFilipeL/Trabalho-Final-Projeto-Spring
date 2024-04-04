89caa28594d778d37c0c824a626233abc62ff43e
lucas <107082216+lucas00314@users.noreply.github.com>
2024-04-04 15:36:07 -0300

    Delete src directory

--- src/main/java/com/example/demo/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
-
